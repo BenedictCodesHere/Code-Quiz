250ee87d9b99ae78453ae217a46280ea0b94754a
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-01-17 13:43:38 +0000

    sorted the nested array by score. Highscores now display in correct order

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,17 +5,23 @@ let nestedScores = JSON.parse(localStorage.getItem("scoreArray"));
 const initialsList = nestedInitials.flat(Infinity);
 const scoresList = nestedScores.flat(Infinity);
 
+const nestedInitialsAndScores = [];
+for (let i = 0; i < initialsList.length; i++) {
+    nestedInitialsAndScores.push([initialsList[i], scoresList[i]]);
+    nestedInitialsAndScores.sort(function(a,b) {
+        return b[1] - a[1]
+    });
+}
+
 function renderHighScores() {
     if (initialsList === "") {
         return;
     } else {
         for (let i = 0; i < initialsList.length; i++) {
-            var todo = `${initialsList[i]}: ${scoresList[i]}`;
+            var todo = `${nestedInitialsAndScores[i][0]}: ${nestedInitialsAndScores[i][1]}`;
             var li = document.createElement("li");
             li.textContent = todo;
             displayHighScores.appendChild(li); 
-         
-            
         }
         
         
